929c00c653ecb0a86f8cb24271fd83befe81ee71
Jake Marsh <jakemmarsh@gmail.com>
2014-10-19 16:49:06 -0400

    Fix directory name in lint task

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -5,7 +5,7 @@ var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-  return gulp.src([config.scripts.src, '!app/javascripts/templates.js'])
+  return gulp.src([config.scripts.src, '!app/js/templates.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
