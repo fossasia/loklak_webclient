eb5fa5906d121e67f920f71059287b25abe8237f
Jake Marsh <jakemmarsh@gmail.com>
2015-02-24 18:36:20 -0500

    Remove redundant watching of JS files

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,8 @@ var gulp          = require('gulp');
 
 gulp.task('watch', ['browserSync', 'server'], function() {
 
-  gulp.watch(config.scripts.src, ['lint', 'browserify']);
+  // Scripts are automatically watched and rebundled by Watchify inside Browserify task
+  gulp.watch(config.scripts.src, ['lint']);
   gulp.watch(config.styles.src,  ['styles']);
   gulp.watch(config.images.src,  ['images', 'reload']);
   gulp.watch(config.views.watch, ['views']);
