c409ccfdf71b5cf3a3510357c8621d7b98424b48
Julian <renard.julian@gmail.com>
2015-05-09 16:04:34 +0200

    Init liveUpdate function

--- app/js/controllers/wall.js
+++ app/js/controllers/wall.js
@@ -6,17 +6,24 @@ var controllersModule = require('./_index');
  * @ngInject
  */
 function WallCtrl($http, AppSettings, SearchService) {
-  var vm = this;
+    var vm = this;
+    vm.term = '';
 
-  vm.update = function(term) {
-  	SearchService.getStatuses(term)
-    	.then(function(statuses) {
-        	vm.statuses = statuses;
-        },
-        function() {
-        	console.log('statuses retrieval failed.');
-        });
-  };
+    var liveUpdate = function() {
+    
+    }
+
+    vm.update = function(term) {
+        vm.term = term;
+        SearchService.getStatuses(term)
+            .then(function(statuses) {
+                vm.statuses = statuses;
+            },
+            function() {
+                console.log('statuses retrieval failed.');
+            });
+    };
+
 }
 
 controllersModule.controller('WallCtrl', WallCtrl);
