f2417b07f4cd1be95bf1e0e0530d2a0ba0bdcfe8
Julian <renard.julian@gmail.com>
2015-05-11 19:33:55 +0200

    Fix duplicate wall routes

--- app/js/routes.js
+++ app/js/routes.js
@@ -35,12 +35,6 @@ function Routes($stateProvider, $locationProvider, $urlRouterProvider, $httpProv
     controller: 'StatisticsCtrl as statistics',
     templateUrl: 'statistics.html',
     title: 'Statistics'
-  })
-  .state('Wall', {
-    url: '/wall',
-    templateUrl: 'wall.html',
-    controller: 'WallCtrl as wall',
-    title: 'Loklak Wall'
   });
 
   $urlRouterProvider.otherwise('/');
