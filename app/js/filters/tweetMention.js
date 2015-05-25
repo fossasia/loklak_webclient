79d892baeb9b622d1b2c1fce4a803327137174f8
fuksi <dinh.tran@metropolia.fi>
2015-05-26 00:13:46 +0300

    Add filters to format hashtags and mentions in tweet text

---
+++ app/js/filters/tweetMention.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var filtersModule = require('./_index.js');
+
+/**
+ * @ngInject
+ */
+
+function tweetMentionFilter() {
+	return function(input) {
+		var mentionReg = /@([\wäöå]+)/gi;
+		var aTag = '<a rel="external" href="https://twitter.com/$1">@$1</a>';
+		return input.replace(mentionReg, aTag);
+	}
+}
+
+
+filtersModule.filter('tweetMention', tweetMentionFilter);
