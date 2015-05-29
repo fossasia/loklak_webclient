4334a1ad721dd64a6d929784fac14668acbd4511
fuksi <dinh.tran@metropolia.fi>
2015-05-29 16:19:37 +0300

    Create a holder for  variable as 'root'

--- app/js/on_run.js
+++ app/js/on_run.js
@@ -5,15 +5,34 @@
  * @ngInject
  */
 function OnRun($rootScope, AppSettings) {
+	var root = {};
+  
+  // Top nav items 
+  root.topNavItems = [
+  		{
+  			'title': 'Home',
+  			'link' : '/'
+  		},
+  		{
+  			'title': 'About',
+  			'link' : '/about'
+  		},
+  		{
+  			'title': 'Search',
+  			'link' : '/search'
+  		},
+	  ];
+
 
   // change page title based on state
   $rootScope.$on('$stateChangeSuccess', function(event, toState) {
-    $rootScope.pageTitle = 'Loklak ';
-
+    var pageTitle = 'Loklak ';
     if ( toState.title ) {
-      $rootScope.pageTitle += toState.title;
+      pageTitle += toState.title;
     }
+    $rootScope.root.pageTitle = pageTitle;
   });
+  $rootScope.root = root;
 
 }
 
