bab9c8bfe8171693a54c3941065baf3469827e30
aneeshd16 <aneeshdevasthale@gmail.com>
2015-06-14 00:26:08 +0530

    Move selection of bigger profile picture into directive

---
+++ app/js/filters/biggerProfileImage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var filtersModule = require('./_index.js');
+
+/**
+ * @ngInject
+ */
+
+function biggerProfileImageFilter() {
+
+    return function(input) {
+      var splitURL = input.split('_bigger');
+      return (splitURL[0] + splitURL[1]);
+    }
+}
+
+filtersModule.filter('biggerProfileImage', biggerProfileImageFilter);
