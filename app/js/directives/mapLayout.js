b43fc3580bfb680132e123760baa8c42537a8ee7
Prashant Kiran <prasht63@gmail.com>
2015-06-20 15:21:57 +0530

    Added markers with Tweet Display, added bounce in animation to the markers

--- app/js/directives/mapLayout.js
+++ app/js/directives/mapLayout.js
@@ -3,6 +3,8 @@
 var directivesModule = require('./_index.js');
 var Leaflet = require('../components/leaflet');
 var GeoJSON = require('../components/geojson');
+var widgets = require('../components/widgets');
+var bouncemarker = require('../components/bouncemarker');
 /**
  * @ngInject
  */
@@ -17,39 +19,45 @@ function mapLayoutDirective() {
         //template : '<p>{{data[0].text}}</p>',
         link: function(scope, element, attrs) {
             scope.$watch('data', function() {
-            	console.log(scope.data);
-            	var result = GeoJSON.parse(scope.data, {
-            	    Point: 'location_point',
-            	    include: ['text']
-            	});
+                console.log(scope.data);
+                var result = GeoJSON.parse(scope.data, {Point: 'location_point' , include:['id_str']}); 
+                console.log(result.features[0]);
+                //clean_slate(); will develop if required to remove the earlier markers
+                add_marker(result);
+                 
+                function add_marker(result) {
+                    var tweetIcon = L.icon({
+                        iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-32.png',
+                    });
+                    console.log(result.features.length);
+                    var marker = [];
+                    var i;
+                    for (i = 0; i < result.features.length; i++) {
+                        console.log("here id ");
+                        var lat=result.features[i].geometry.coordinates[1];
+                        var lng=result.features[i].geometry.coordinates[0];
+                        marker[i] = new L.Marker([lat, lng], {
+                            id: result.features[i].properties.id_str,
+                            icon:tweetIcon,
+                            bounceOnAdd: true
+                        });
+                    marker[i].addTo(map);
+                    marker[i].on('click', showTweet);
+                    };
+                }
 
-            	function onEachFeature(layer, feature) {
-            	    if (feature.properties && feature.properties.style) {
-            	        var popupContent = feature.properties.text[0];
+          
+                function showTweet(e){   
+                    document.getElementById('TweetBox').innerHTML="";
+                    var id=this.options.id; 
+                    twttr.widgets.createTweet(id,
+                    document.getElementById('TweetBox'),{
+                        theme: 'light'
+                    }
+                    );
+                }
 
-            	        console.log(popupContent + "is");
-            	    }
-            	}
 
-            	L.geoJson([result], {
-
-            	    style: function(feature) {
-            	        return feature.properties && feature.properties.style;
-            	    },
-
-            	    onEachFeature: onEachFeature,
-
-            	    pointToLayer: function(feature, latlng) {
-            	        return L.circleMarker(latlng, {
-            	            radius: 8,
-            	            fillColor: "#ff7800",
-            	            color: "#000",
-            	            weight: 1,
-            	            opacity: 1,
-            	            fillOpacity: 0.8
-            	        });
-            	    }
-            	}).addTo(map);
             }, true);
             var map = L.map(attrs.id).setView([2.252776, 48.845261], 2);
             L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
