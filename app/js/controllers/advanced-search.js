4d497b1e63b2f3ed43d8299b07976d5c5446f69a
fuksi <dinh.tran@metropolia.fi>
2015-06-23 09:34:01 +0300

    Init remove initialzation for until and since, add reset & search button

--- app/js/controllers/advanced-search.js
+++ app/js/controllers/advanced-search.js
@@ -5,8 +5,10 @@ var controllersModule = require('./_index');
 function AdvancedSearchCtrl($http, AppSettings, SearchService) {
 
 	var vm = this;
-	vm.sinceDate = new Date();
-	vm.untilDate = new Date();  		
+	// Init period to search - 6 month
+/*	vm.sinceDate = new Date();
+	vm.sinceDate.setMonth(getMonth()-6);
+	vm.untilDate = new Date();*/  		
 	vm.foo = "foo";
 }
 
