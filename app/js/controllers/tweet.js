4e11096ce8caa5f7e4b1d233fe24ac1b9af1c5d6
sudheesh001 <sudheesh1995@outlook.com>
2015-06-28 13:45:01 +0530

    Fixes #189 Enables live tweet text validations character counter to show up

--- app/js/controllers/tweet.js
+++ app/js/controllers/tweet.js
@@ -1,19 +1,29 @@
 'use strict';
 
 var controllersModule = require('./_index');
-
+var twitterText = require('twitter-text');
 
 controllersModule.controller('HomeCtrl', ['$rootScope', 'HelloService', function($rootScope, hello) {
 
     $rootScope.root.tweet= "";
+    $rootScope.root.tweetLength = 140;
+    console.log($rootScope.root.tweetLength);
     $rootScope.root.foo = function() 
     {    
         var message = $rootScope.root.tweet;
+        var tweetLen = twttr.txt.getTweetLength($rootScope.root.tweet);
         var tweet = encodeURIComponent(message);
         console.log(message);
-        hello('twitter').api('me/share', 'POST', {
-            message : tweet
-        });
+        console.log(tweetLen);
+        // hello('twitter').api('me/share', 'POST', {
+        //     message : tweet
+        // });
     };
+
+    $rootScope.root.tweetLengthCalculate = function() {
+        var tweet = $rootScope.root.tweet;
+        $rootScope.root.tweetLength = 140 - twttr.txt.getTweetLength(tweet);
+        console.log($rootScope.root.tweetLength);
+    }
 
 }]);
