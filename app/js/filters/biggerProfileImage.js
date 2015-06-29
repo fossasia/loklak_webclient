efc692fc726d6a81d9f3adaad690ef194e850366
sudheesh001 <sudheesh1995@outlook.com>
2015-06-29 23:14:10 +0530

    Adds filters to the missing cards, coa and linear layouts on wall to show links clickable

--- app/js/filters/biggerProfileImage.js
+++ app/js/filters/biggerProfileImage.js
@@ -11,7 +11,7 @@ function biggerProfileImageFilter() {
     return function(input) {
       var splitURL = input.split('_bigger');
       return (splitURL[0] + splitURL[1]);
-    }
+    };
 }
 
 filtersModule.filter('biggerProfileImage', biggerProfileImageFilter);
