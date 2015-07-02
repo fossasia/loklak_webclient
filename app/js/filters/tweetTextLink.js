fbab404343be2cbe850a905119c426f8a118fdd0
aneeshd16 <aneeshdevasthale@gmail.com>
2015-07-02 23:37:07 +0530

    Solves #253

--- app/js/filters/tweetTextLink.js
+++ app/js/filters/tweetTextLink.js
@@ -6,10 +6,18 @@ var filtersModule = require('./_index.js');
  * @ngInject
  */
 
- filtersModule.filter('tweetTextLink', function() {
- 	return function(input) {
- 		var aTag = '<a class="external-link" rel="external" href="$1">$1</a>';
- 		var lastLinkReg = /((http)\S+)/gi;
- 		return input.replace(lastLinkReg, aTag);
- 	};
- });
+filtersModule.filter('tweetTextLink', function() {
+    return function(input) {
+        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(expression);
+        var split = input.split(" ");
+        for (var i = 0; i < split.length; i++) {
+            if (split[i].match(regex)) {
+                var text = split[i].split(".").slice(1).join(".").split("/")[0];
+
+                split[i] = '<a href=\"' + split[i] + '\">' + text + '...</a>';
+            }
+        }
+        return split.join(" ");
+    };
+});
