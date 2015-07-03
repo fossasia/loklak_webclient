f40440dbffcf70503633ebecccf0be16cdd2bba4
aneeshd16 <aneeshdevasthale@gmail.com>
2015-07-03 17:53:51 +0530

    Solves #253

--- app/js/filters/tweetTextLink.js
+++ app/js/filters/tweetTextLink.js
@@ -8,16 +8,16 @@ var filtersModule = require('./_index.js');
 
 filtersModule.filter('tweetTextLink', function() {
     return function(input) {
-        var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-        var regex = new RegExp(expression);
-        var split = input.split(" ");
-        for (var i = 0; i < split.length; i++) {
-            if (split[i].match(regex)) {
-                var text = split[i].split(".").slice(1).join(".").split("/")[0];
-
-                split[i] = '<a href=\"' + split[i] + '\">' + text + '...</a>';
+        function geturl(url) {
+            if (url.length > 22) {
+                return url.substr(0, 22) + "...";
+            } else {
+                return url;
             }
         }
-        return split.join(" ");
+        var urlRegex = /(https?:\/\/[^\s]+)/g;
+        return input.replace(urlRegex, function(url) {
+            return '<a href="' + url + '">' + geturl(url) + '</a>';
+        })
     };
 });
