fcd776f287d0a358e046da592d62a9315a75ad59
sudheesh001 <sudheesh1995@outlook.com>
2015-07-04 20:49:07 +0530

    Fixes #286 Allows non english character based hashtags to be formed

--- app/js/filters/tweetHashtag.js
+++ app/js/filters/tweetHashtag.js
@@ -8,7 +8,7 @@ var filtersModule = require('./_index.js');
 
  filtersModule.filter('tweetHashtag', function () {
  	return function(input) {
- 		var aTag = '<a class="external-hashtag" rel="external" href="./search?q=%23$1">#$1</a>';
+ 		var aTag = '<a class="external-hashtag" rel="external" href="./search?q=%23$1">#$1</a>&nbsp;';
  		var hashtagReg = /#([^#^\ ^@]+)[\s,;]*/gi;
  		return input.replace(hashtagReg, aTag);
  	};
