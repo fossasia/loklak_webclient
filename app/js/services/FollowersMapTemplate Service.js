97c1902147167e97ff1d45f0239a2bfb22759698
Prashant Kiran <prasht63@gmail.com>
2015-07-11 00:05:53 +0530

    Added content for following too with description about followers/following

--- app/js/services/FollowersMapTemplate Service.js
+++ app/js/services/FollowersMapTemplate Service.js
@@ -9,33 +9,74 @@ function FollowersMapTemplateService($filter) {
 
   var service = {};
 
-  service.genStaticTwitterFollower = function(status) {
-  	var result = "";
-  	var text = $filter('tweetHashtag')($filter('tweetMention')($filter('tweetTextLink')(status.text)));
-  	var created_at = $filter('date')(status.created_at, 'dd MMM yyyy');
-  	var result='<div class="single-tweet tweet map-tweet">'
-  		+			 '<div class="top-metadata">'
-  		+				'<img class="avatar"' 
-  		+					 'onError="this.onerror=null;this.src=\'/images/anon_200x200.png\';"'
-  		+					 'src="' + status.user.profile_image_url_https + '" alt="user-images">'
-  		+			   '<div class="user-data-wrapper">'
-  		+			    	'<a class="name" href="' + 'https://twitter.com/' + status.user.screen_name + '">' + status.user.name + '</a>'			
-  		+			   	'<span class="screen-name">' + '@' + status.user.screen_name + '</span>'	
-  		+			    '</div>'
-  		+			'</div>'
-  		+ 			'<p class="tweet-content-text">' + text +  '</p>'
-  		+				'<div class="detail-metadata">'
-  		+			    	'<span class="metadata">' + created_at + '</span> · '
-  		+				'</div>'        
-  		+			'</div>'
-  		+		'</div>'
+  service.genStaticTwitterFollower = function(followers , i) {
+    var result = "";
+    console.log("Banner photo is"+followers.profile_banner[i]);
+    result = '<div class="left-result-container col-lg-12 col-md-12 col-sm-12">'
+            +'<div class="home-user-info">'
+            +   '<div class="blue-background-placeholder">'
+            +        '<img src="'+followers.profile_banner[i]+'" >'    
+            +    '</div>'
+            +     '<div class="home-user-info-content">'
+            +        '<div class="top-content">'
+            +            '<img src="'+followers.propic[i]+'">'
+            +            '<div class="name-and-screen-name">'
+            +                '<span class="home-user-name">'+followers.name[i]+'</span><br>'
+            +               '<span class="home-user-screen-name">@'+followers.screenname[i]+'</span>'
+            +            '</div>'
+            +       '</div>'
+            +       '<div class="bottom-content">'
+            +            '<div class="user-no-tweets">'
+            +               '<a href="">Tweets<br><span>'+followers.tweetcount[i]+'</span></a>'
+            +            '</div>'
+            +            '<div class="user-no-followings">'
+            +                '<a href="">Followings<br><span>'+followers.following[i]+'</span></a>'
+            +            '</div>'
+            +            '<div class="user-no-followers">'
+            +                '<a href="">Followers<br><span>'+followers.followers[i]+'</span></a>'
+            +            '</div>'
+            +        '</div>'
+            +    '</div>'
+            +'</div>'
 
-
-  	return result;
+    return result;
 
 
   };
+ service.genStaticTwitterFollowing = function(following , i) {
+    var result = "";
+    console.log("Banner photo is"+following.profile_banner[i]);
+    result = '<div class="left-result-container" ng-show="root.twitterSession">'
+            +'<div class="home-user-info">'
+            +   '<div class="blue-background-placeholder">'
+            +        '<img src="'+following.profile_banner[i]+'" >'    
+            +    '</div>'
+            +     '<div class="home-user-info-content">'
+            +        '<div class="top-content">'
+            +            '<img src="'+following.propic[i]+'">'
+            +            '<div class="name-and-screen-name">'
+            +                '<span class="home-user-name">'+following.name[i]+'</span><br>'
+            +               '<span class="home-user-screen-name">@'+following.screenname[i]+'</span>'
+            +            '</div>'
+            +       '</div>'
+            +       '<div class="bottom-content">'
+            +            '<div class="user-no-tweets">'
+            +               '<a href="">Tweets<br><span>'+following.tweetcount[i]+'</span></a>'
+            +            '</div>'
+            +            '<div class="user-no-followings">'
+            +                '<a href="">Followings<br><span>'+following.following[i]+'</span></a>'
+            +            '</div>'
+            +            '<div class="user-no-followers">'
+            +                '<a href="">Followers<br><span>'+following.followers[i]+'</span></a>'
+            +            '</div>'
+            +        '</div>'
+            +    '</div>'
+            +'</div>'
 
+    return result;
+
+
+  };
 
 
 
