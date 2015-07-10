7b701ee302c7034031948f3a23f1b5f56d99caee
fuksi <dinh.tran@metropolia.fi>
2015-07-10 14:42:09 +0300

    Add basic functionality to retweet without comments

--- app/js/directives/retweet-modal.js
+++ app/js/directives/retweet-modal.js
@@ -5,8 +5,21 @@ var directivesModule = require('./_index.js');
 directivesModule.directive("retweetModal", ["$rootScope", function($rootScope) {
 
 	return {
+		scope: {},
 		templateUrl: "retweet-modal.html",
 		controller: function($scope, $element, $attrs) {
+			$scope.confirmRt = function() {
+				var id = $rootScope.root.currentTweet.id_str;
+				$rootScope.root.hello('twitter').api('me/share', 'POST', {
+			        id : id
+			    }).then(function(json) {
+                      console.log(json);
+                  }, function(e) {
+                      console.log(e);
+                  });;
+
+			    angular.element(".retweet-modal").addClass("hide");
+			}
 		}
 	};
 }]);
