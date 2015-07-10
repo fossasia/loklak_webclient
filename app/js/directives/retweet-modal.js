397ff6839941b5b51c41097463b1a4861bcecc8d
fuksi <dinh.tran@metropolia.fi>
2015-07-10 16:35:45 +0300

    Add limits of chars, add tweet with comment fn

--- app/js/directives/retweet-modal.js
+++ app/js/directives/retweet-modal.js
@@ -8,25 +8,73 @@ directivesModule.directive("retweetModal", ["$rootScope", function($rootScope) {
 		scope: true,
 		templateUrl: "retweet-modal.html",
 		controller: function($scope, $element, $attrs) {
-			
+			$scope.comment = "";
+			$scope.remainingChars = 140;
 			$scope.status = {};
+
+			// Max-length = comment.length + tweet.link
 			$scope.$watch(function() {
 				return $rootScope.root.currentTweet;
 			}, function(value) {
 				$scope.status = value;
+				$scope.remainingChars -= value.link.length;
 			});
-			
-			$scope.confirmRt = function() {
-				var id = $rootScope.root.currentTweet.id_str;
-				$rootScope.root.hello('twitter').api('me/share', 'POST', {
-			        id : id
-			    }).then(function(json) {
-                      console.log(json);
-                  }, function(e) {
-                      console.log(e);
-                  });;
 
-			    angular.element(".retweet-modal").addClass("hide");
+			// Watching comment.length
+
+			$scope.$watch(function() {
+				return $scope.comment;
+			}, function() {
+				$scope.remainingChars = 140 - $scope.status.link.length - $scope.comment.length;
+				if ($scope.remainingChars <= 0) {
+					angular.element(".remaining-chars").addClass("red");
+					angular.element(".retweet-button").addClass("disabled");
+				} else {
+					if (angular.element(".remaining-chars").hasClass("red")) {
+						angular.element(".remaining-chars").removeClass("red");
+					}
+					if (angular.element(".retweet-button").hasClass("disabled")) {
+						angular.element(".retweet-button").removeClass("disabled");
+					}
+				}
+			})
+			
+			// Click on textarea will expand it
+			$scope.showMoreCols = function() {
+				angular.element(".retweet-modal .retweet-comment textarea").addClass("active");				
+			}
+
+			$scope.closeRtModal = function() {
+				angular.element(".retweet-modal").addClass("hide");				
+			}
+
+			// Retweet action
+			$scope.confirmRt = function() {
+				if (!angular.element(".retweet-button").hasClass("disabled")) {
+					// RT with comment
+					if ($scope.comment) {
+						var tweet = $scope.comment + " " + $scope.status.link;
+						$rootScope.root.hello('twitter').api('me/share', 'POST', {
+				        	message : tweet
+					    }).then(function(json) {
+							console.log(json);
+						}, function(e) {
+							console.log(e);
+						});;
+						angular.element(".retweet-modal").addClass("hide");
+						$scope.comment = "";
+					} else { // Just RT
+						var id = $rootScope.root.currentTweet.id_str;
+						$rootScope.root.hello('twitter').api('me/share', 'POST', {
+					        id : id
+					    }).then(function(json) {
+		                      console.log(json);
+		                  }, function(e) {
+		                      console.log(e);
+		                  });;
+					    angular.element(".retweet-modal").addClass("hide");
+					}
+				}
 			}
 		}
 	};
