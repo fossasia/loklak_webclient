ac22b9f150001d74dcbcc83772868019d0420c94
fuksi <dinh.tran@metropolia.fi>
2015-07-17 01:48:32 +0300

    Add style to modal-content, auto focus and select link ready to copy

--- app/js/directives/copy-link-modal.js
+++ app/js/directives/copy-link-modal.js
@@ -2,14 +2,20 @@
 
 var directivesModule = require('./_index.js');
 
-directivesModule.directive("copyLinkModal", ["$rootScope", function($rootScope) {
+directivesModule.directive("copyLinkModal", ["$rootScope", "$location", "$timeout", function($rootScope, $location, $timeout) {
 
 	return {
 		scope: true,
 		templateUrl: "copy-link-modal.html",
 		controller: function($scope, $element, $attrs) {
 			$rootScope.root.openPromptToCopyLink = function(id) {
-				console.log(id);
+				$scope.promptLink = window.location.origin +  "/search?q=id:" + id;
+				angular.element(".copy-link-modal").removeClass("hide");
+				var input = document.getElementById("link-to-copy-container");
+				$timeout(function() {
+					input.setSelectionRange(0, input.value.length);
+				}, 0);
+				input.focus();
 			}
 		}
 	};
