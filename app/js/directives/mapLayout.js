f506dd1d580ef556a23e70537694aed7796c0473
aneeshd16 <aneeshdevasthale@gmail.com>
2015-08-08 13:23:38 +0530

    #431 Change height of map box automatically

--- app/js/directives/mapLayout.js
+++ app/js/directives/mapLayout.js
@@ -9,7 +9,7 @@ var bouncemarker = require('../components/bouncemarker');
 /**
  * @ngInject
  */
-function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationService) {
+function mapLayoutDirective(MapPopUpTemplateService, $interval) {
 
     return {
         scope: {
@@ -22,8 +22,10 @@ function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationServi
             var curr = 0;
             var marker = [];
 
+            element.height(0.75 * $(window).height());
+
             var map = L.map(attrs.id).setView([2.252776, 48.845261], 2);
-            L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                 maxZoom: 18,
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
                     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -31,6 +33,12 @@ function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationServi
                 id: 'examples.map-20v6611k'
             }).addTo(map);
 
+            map.on('popupopen', function(e) {
+                var px = map.project(e.popup._latlng); // find the pixel location on the map where the popup anchor is
+                px.y -= e.popup._container.clientHeight/2 // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
+                map.panTo(map.unproject(px),{animate: true}); // pan to new center
+            });
+            
             function contains(marker, elem) {
                 for (var m in marker) {
                     if (m.id === elem.id_str) {
@@ -51,6 +59,7 @@ function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationServi
                 scope.data.forEach(function(ele) {
                     if (!contains(marker, ele)) {
                         if (ele.location_mark) {
+                            console.log(ele.location_mark);
                             var text = MapPopUpTemplateService.genStaticTwitterStatus(ele);
                             var pointObject = {
                                 "geometry": {
@@ -62,7 +71,7 @@ function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationServi
                                 },
                                 "type": "Feature",
                                 "properties": {
-                                    "popupContent": "<div class='foobar'>" + text + "</div>"
+                                    "popupContent": text
                                 },
                                 "id": ele.id_str
                             };
@@ -77,11 +86,11 @@ function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationServi
                                 icon: tweetIcon,
                                 bounceOnAdd: true
                             });
-                            tempMarker.addTo(map);
-                            var popup = L.popup({
-                                autoPan: false
-                            }).setContent(pointObject.properties.popupContent);
-                            tempMarker.bindPopup(popup);
+                            tempMarker.addTo(map).bindPopup("<div class='foobar'>" + text + "</div>");//.openPopup();
+                            // var popup = L.popup({
+                            //     autoPan: true
+                            // }).setContent(pointObject.properties.popupContent);
+                            // tempMarker.bindPopup(popup);
                             marker.push(tempMarker);
                         }
                     }
@@ -95,7 +104,6 @@ function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationServi
                     var tweetIcon = L.icon({
                         iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-32.png',
                     });
-                    console.log(result.features.length);
                     var i;
                     for (i = 0; i < result.features.length; i++) {
                         console.log("here id ");
@@ -149,4 +157,4 @@ function mapLayoutDirective(MapPopUpTemplateService, $interval, MapCreationServi
 
 }
 
-directivesModule.directive('maplayout', ['MapPopUpTemplateService', '$interval', 'MapCreationService', mapLayoutDirective]);
+directivesModule.directive('maplayout', ['MapPopUpTemplateService', '$interval', mapLayoutDirective]);
