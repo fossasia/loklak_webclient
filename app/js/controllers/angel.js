ae20d3298f7a5d8944abeab56c606bed8c23295a
Prashant Kiran <prasht63@gmail.com>
2015-08-10 11:45:12 +0530

    Retrieved startups from Angel.co

--- app/js/controllers/angel.js
+++ app/js/controllers/angel.js
@@ -12,6 +12,8 @@ var marker=[];
  */
 
  controllersModule.controller('AngelCtrl', ['$rootScope','$http','$scope','AppSettings', function($rootScope,$http,$scope,AppSettings) {
+  var totalpage=0;
+  var currentpage=0;
     
 var code = function () {
   // This function is anonymous, is executed immediately and 
@@ -35,24 +37,62 @@ var code = function () {
   } 
     return query_string;
 }();
-$http.post('https://angel.co/api/oauth/token?callback=JSON_CALLBACK', { params:
+$http.jsonp('https://angel.co/api/oauth/token?callback=JSON_CALLBACK', { params:
     {client_id:'f148a48d308ee0ee11eb938a2f2f88bff24abb235412f596',
     client_secret:'45b13fc4264976a051f40e4bbc62b91e0d0b50999ff9b089',
     code : code,
     grant_type:'authorization_code'
     }}).
   then(function(response) {
-    //console.log("Sent a request to guardian angel");
-    document.getElementById("data").innerHTML=response.data;
     console.log(response);
+    
   }, function(response) {
-    console.log("Sent a reqwwuest to guardian angel");
+    
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+  $scope.curr_page=0;
+  $scope.lastpage;
+ 
+
+    var url="https://api.angel.co/1/startups?filter=raising&access_token=8cd27d851f1e4b69e27a29cc9606822c118586d4c8b168b5&callback=JSON_CALLBACK";
+    console.log(url);
+    $http.jsonp(url).
+    then(function(response) {
+      $scope.curr_page=response.data.page;
+      $scope.lastpage=response.data.last_page;
+      getStartups($scope.lastpage);
+    
+  }, function(response) {
+    
+    
+    console.log(response);
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+
+  function getStartups(lastpage)
+  {
+    var i=1;
+    for(i=1;i<=lastpage;i++)
+    {
+      var url="https://api.angel.co/1/startups?filter=raising&access_token=8cd27d851f1e4b69e27a29cc9606822c118586d4c8b168b5&callback=JSON_CALLBACK&page="+i;
+      $http.jsonp(url).
+    then(function(response) {
+      console.log(response.data.page);
+  }, function(response) {
+    
     document.getElementById("data").innerHTML=response.data;
     console.log(response);
     // called asynchronously if an error occurs
     // or server returns response with an error status.
   });
-console.log(code);
+
+    }
+  }
+
+
+  
       
 
 
