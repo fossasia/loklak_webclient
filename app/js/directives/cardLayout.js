34a4440e05fb7bbcce0396250d7b561025795bab
aneeshd16 <aneeshdevasthale@gmail.com>
2015-08-12 22:00:28 +0530

    Changed directive declaration method #319

--- app/js/directives/cardLayout.js
+++ app/js/directives/cardLayout.js
@@ -1,34 +1,28 @@
 'use strict';
+/* jshint unused:false */
 
 var directivesModule = require('./_index.js');
 
-/**
- * @ngInject
- */
-function cardLayout() {
-
-  return {
-    scope: {
-    	data: '=',
-    },
-    templateUrl: 'wall/templates/card.html',
-    controller: function($scope) {
-    	$scope.getClass = function () {
-    		if($scope.data.images.length>0){
-    			return 'col-xs-6';
-    		}
-    		else {
-    			if($scope.data.text.length>70) {
-    				return 'col-xs-4';
-    			}
-    			else {
-    				return 'col-xs-3';
-    			}
-    		}
-    	}
-    }
-  };
-
-}
-
-directivesModule.directive('card', cardLayout);
+directivesModule.directive('card', [function() {
+	return {
+	  scope: {
+	  	data: '=',
+	  },
+	  templateUrl: 'wall/templates/card.html',
+	  controller: function($scope) {
+	  	$scope.getClass = function () {
+	  		if($scope.data.images.length>0){
+	  			return 'col-xs-6';
+	  		}
+	  		else {
+	  			if($scope.data.text.length>70) {
+	  				return 'col-xs-4';
+	  			}
+	  			else {
+	  				return 'col-xs-3';
+	  			}
+	  		}
+	  	}
+	  }
+	};
+}]);
