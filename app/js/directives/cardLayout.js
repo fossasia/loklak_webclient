5f935031bf910e3637c31d548ba66f6aadf04dc1
aneeshd16 <aneeshdevasthale@gmail.com>
2015-08-12 20:27:52 +0530

    #319 change width based on tweet text length

--- app/js/directives/cardLayout.js
+++ app/js/directives/cardLayout.js
@@ -12,6 +12,21 @@ function cardLayout() {
     	data: '=',
     },
     templateUrl: 'wall/templates/card.html',
+    controller: function($scope) {
+    	$scope.getClass = function () {
+    		if($scope.data.images.length>0){
+    			return 'col-xs-6';
+    		}
+    		else {
+    			if($scope.data.text.length>70) {
+    				return 'col-xs-4';
+    			}
+    			else {
+    				return 'col-xs-3';
+    			}
+    		}
+    	}
+    }
   };
 
 }
