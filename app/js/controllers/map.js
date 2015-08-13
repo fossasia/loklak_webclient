e0c2c278fa5122bf8c80e1c2d289c92b211b94f6
fuksi <dinh.tran@metropolia.fi>
2015-08-14 01:57:19 +0300

    Add button to toggle map/timeline

--- app/js/controllers/map.js
+++ app/js/controllers/map.js
@@ -11,100 +11,109 @@ controllersModule.controller('MapCtrl', [ '$rootScope', '$scope', 'MapCreationSe
     var vm = this;
     vm.feedLimit = 20;
     vm.failNoticeShown = false;
-
-    /*
-     * Since the map creation process is abstracted to be reused across all view
-     * , a 'user' property is needed for the map creation
-     */
-    function addUserProp(topology) {
-        topology.forEach(function(ele) {
-            ele.user = {
-                "screen_name" : ele.screen_name,
-                "user_id" : ele.id_str,
-                "name" : ele.name,
-                "profile_image_url_https" : ele.profile_image_url_https,
-            };
-        })
-    }    
-
-    /*
-     * Stimulate a marker's clicking's trigger
-     * userID of feed will be matched with id of the marker
-     * toggle notice if marker is not available
-     */
-    vm.openPopup = function(userId) {
-        if (window.mapViewMarker[userId]) {
-            $(".location-fail-notice").fadeOut();
-            vm.failNoticeShown = false;
-
-            window.map.panTo(window.mapViewMarker[userId]._latlng);
-            window.mapViewMarker[userId].openPopup();    
-            
-        } else {
-            if (vm.failNoticeShown) { $(".location-fail-notice").fadeOut(200).fadeIn(200); } 
-            window.map.closePopup();
-            $(".location-fail-notice").fadeIn();
-            vm.failNoticeShown = true;
-        } 
-    }
+    vm.isShowingMapNotHome = true;
 
 
-    // START MAP WHEN DATA IS RETURNED
-    $rootScope.$watch(function() {
-        return $rootScope.userTopology;
-    }, function(val) {
-        if (val) {
-            if (window.map) { delete(window.map); }
-    
-            var topologyPool = $rootScope.userTopology.followers.concat($rootScope.userTopology.following);
-            addUserProp(topologyPool);
-            MapCreationService.initMap({
-                data: topologyPool,
-                mapId: "map",
-                templateEngine: "genUserInfoPopUp",
-                markerType: "userAvatar",
-                cbOnMapAction: function() { 
-                    var hintText = '<p class="map-control-hint-text">The first number shows the actual followers. The second number shows the number of followers with a location info. These are displayed on the map.</p>';
-                    $(".leaflet-control-layers-overlays").append(hintText);
-                    $(".map-control-hint").hover(function() {
-                        $(".map-control-hint-text").show();
-                    }, function() {
-                        $(".map-control-hint-text").hide();
-                    });
-                }
-            });                
+    /* MAP VIEW MODELS */
+        /*
+         * Since the map creation process is abstracted to be reused across all view
+         * , a 'user' property is needed for the map creation
+         */
+        function addUserProp(topology) {
+            topology.forEach(function(ele) {
+                ele.user = {
+                    "screen_name" : ele.screen_name,
+                    "user_id" : ele.id_str,
+                    "name" : ele.name,
+                    "profile_image_url_https" : ele.profile_image_url_https,
+                };
+            })
+        }    
+
+        /*
+         * Stimulate a marker's clicking's trigger
+         * userID of feed will be matched with id of the marker
+         * toggle notice if marker is not available
+         */
+        vm.openPopup = function(userId) {
+            if (window.mapViewMarker[userId]) {
+                $(".location-fail-notice").fadeOut();
+                vm.failNoticeShown = false;
+
+                window.map.panTo(window.mapViewMarker[userId]._latlng);
+                window.mapViewMarker[userId].openPopup();    
+                
+            } else {
+                if (vm.failNoticeShown) { $(".location-fail-notice").fadeOut(200).fadeIn(200); } 
+                window.map.closePopup();
+                $(".location-fail-notice").fadeIn();
+                vm.failNoticeShown = true;
+            } 
         }
-    })
 
-    /*
-     * Manual code for scroll down to load more feature
-     * The basis for this implementation is based on how
-     * ele's clientHeight, scrollHeight & scrollTop is calculated
-     * https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight
-     */
-    function hasReachBottom() {
-        var element = document.getElementsByClassName("activity-feed")[0];
-        var maxScroll = element.scrollHeight - element.clientHeight - 200;
-        var currentScrollTop = $(".activity-feed").scrollTop();
-        return currentScrollTop >= maxScroll ? true : false;
-    }
 
-    angular.element(document).ready(function() {
-        $(".activity-feed").scroll(function() {
-            if (hasReachBottom()) {
-                $scope.$apply(function() {
-                    vm.feedLimit += 20;    
-                });
-                console.log("Foo");
+        // START MAP WHEN DATA IS RETURNED
+        $rootScope.$watch(function() {
+            return $rootScope.userTopology;
+        }, function(val) {
+            if (val) {
+                if (window.map) { delete(window.map); }
+        
+                var topologyPool = $rootScope.userTopology.followers.concat($rootScope.userTopology.following);
+                addUserProp(topologyPool);
+                MapCreationService.initMap({
+                    data: topologyPool,
+                    mapId: "map",
+                    templateEngine: "genUserInfoPopUp",
+                    markerType: "userAvatar",
+                    cbOnMapAction: function() { 
+                        var hintText = '<p class="map-control-hint-text">The first number shows the actual followers. The second number shows the number of followers with a location info. These are displayed on the map.</p>';
+                        $(".leaflet-control-layers-overlays").append(hintText);
+                        $(".map-control-hint").hover(function() {
+                            $(".map-control-hint-text").show();
+                        }, function() {
+                            $(".map-control-hint-text").hide();
+                        });
+                    }
+                });                
             }
-        });
-    })
+        })
+
+        /*
+         * Manual code for scroll down to load more feature
+         * The basis for this implementation is based on how
+         * ele's clientHeight, scrollHeight & scrollTop is calculated
+         * https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight
+         */
+        function hasReachBottom() {
+            var element = document.getElementsByClassName("activity-feed")[0];
+            var maxScroll = element.scrollHeight - element.clientHeight - 200;
+            var currentScrollTop = $(".activity-feed").scrollTop();
+            return currentScrollTop >= maxScroll ? true : false;
+        }
+
+        angular.element(document).ready(function() {
+            $(".activity-feed").scroll(function() {
+                if (hasReachBottom()) {
+                    $scope.$apply(function() {
+                        vm.feedLimit += 20;    
+                    });
+                    console.log("Foo");
+                }
+            });
+        })
 
 
-    /*
-     * Toggle map feed to slide left/hide or slide right/show
-    */
-    vm.toggleMapFeed = function() {
-        angular.element('.activity-feed').toggleClass("show-feed");
+        /*
+         * Toggle map feed to slide left/hide or slide right/show
+        */
+        vm.toggleMapFeed = function() {
+            angular.element('.activity-feed').toggleClass("show-feed");
+        }
+
+    /* SWITCHING BETWEEN TIMELINE AND MAP */
+    vm.toggleMapAndTimeline = function() {
+        vm.isShowingMapNotHome = !vm.isShowingMapNotHome;        
     }
+
 }]);
