ff3def9cb78bfecb35c1ef7c5934beb9896826b0
zyzo <danghaian168@gmail.com>
2015-08-19 10:15:11 +0700

    Add source type select input when source format is geojson
    Make a clear distinction between source type & source format : source format is the data format, while source type is the type of data saved into loklak. e.g. A data of type fossasia api can be imported using geojson format.

--- app/js/directives/add-connection-modal.js
+++ app/js/directives/add-connection-modal.js
@@ -14,17 +14,14 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 			$scope.sourceTypeList = SourceTypeService.sourceTypeList;
 			$scope.sourceTypeListWEndpoint = {};
 			$scope.loklakFields = LoklakFieldService.fieldList;
-			// Form inputs
-			$scope.inputs = { mapRules : {}};
-			// Default values
-			$scope.inputs.harvesting_freq = {'value': 360, 'label':'6 hours'};
-			$scope.inputs.public = true;
-			// Submit validation messages
-			$scope.messages = {};
-
+			
 			const sourceTypeFields = LoklakFieldService.sourceTypeFields;
 			// List of fields that user can map data to. Depending on selected source type
 			$scope.currentLoklakFields = null;
+			// Form inputs
+			$scope.inputs = { mapRules : {}};
+			// Submit validation messages
+			$scope.messages = {};
 
 			for (var key in $scope.sourceTypeList) {
 				if ($scope.sourceTypeList[key].endpoint) {
@@ -41,9 +38,9 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 
 			$scope.tabItems = [
 				{
-					'title' : 'Source Type',
+					'title' : 'Source Format',
 					'icon' : 'fa fa-file-code-o',
-					'target' : 'source-type-tab'
+					'target' : 'source-format-tab'
 				},
 				{
 					'title' : 'Source URL & Info',
@@ -59,30 +56,23 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 			$scope.selectedTab = 0;
 			$scope.showNext = true;
 
-			for (var key in $scope.loklakFields) {
-				if (!$scope.inputs.mapRules[key]) {
-					$scope.inputs.mapRules[key] = [];
-				}
-				$scope.inputs.mapRules[key][1] = $scope.loklakFields[key].value; // fill second column with loklak fields
-			}
-
 			$scope.closeSettingModal = function() {
 				angular.element("#add-connect-setting-modal").css('display', 'none');
 				angular.element(".modal-backdrop").remove();
 			};
 
-			$scope.setSourceType = function(e) {
-				$scope.inputs.sourceType = e.currentTarget.id;
+			$scope.setSourceFormat = function(e) {
+				$scope.inputs.sourceFormat = e.currentTarget.id;
 				$scope.proceed();
 				// refresh validation state
 				$scope.validateSourceUrl();
 
 				// pick only map rules that apply for this source type
 				$scope.currentLoklakFields = {};
-				if (sourceTypeFields[$scope.inputs.sourceType]
-					&& sourceTypeFields[$scope.inputs.sourceType].length != 0) {
-					for (var key in sourceTypeFields[$scope.inputs.sourceType]) {
-						var data = sourceTypeFields[$scope.inputs.sourceType][key];
+				if (sourceTypeFields[$scope.inputs.sourceFormat]
+					&& sourceTypeFields[$scope.inputs.sourceFormat].length != 0) {
+					for (var key in sourceTypeFields[$scope.inputs.sourceFormat]) {
+						var data = sourceTypeFields[$scope.inputs.sourceFormat][key];
 						$scope.currentLoklakFields[data] = $scope.loklakFields[data];
 					}
 				} else {
@@ -114,7 +104,7 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 					$scope.messages.error = 'Please provide a valid source url';
 					return;
 				}
-				if (!$scope.inputs.sourceType) {
+				if (!$scope.inputs.sourceFormat) {
 					$scope.messages.error = 'Please select a data source format';
 					return;
 				}
@@ -122,10 +112,10 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 					var mapRulesStr = '';
 					const mapRules = $scope.inputs.mapRules;
 					var prefix = '';
-					for (var key in $scope.inputs.mapRules) {
+					for (var key in mapRules) {
 						var data = $scope.inputs.mapRules[key];
 						if (data[0] && data[0].length > 0) {
-							mapRulesStr += prefix + mapRules[i][0] + ':' + mapRules[i][1];
+							mapRulesStr += prefix + mapRules[key][0] + ':' + mapRules[key][1];
 							prefix = ',';
 						}
 					}
@@ -134,10 +124,14 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 				var lifetime = null;
 				if ($scope.inputs.lifetime)
 					lifetime = new Date($scope.inputs.lifetime).getTime();
-				if ($scope.inputs.sourceType === 'geojson') {
+				if ($scope.inputs.sourceFormat === 'geojson') {
+					if (!$scope.inputs.sourceType) {
+						$scope.messages.error = 'Please select a source type';
+						return;
+					}
 					PushService.pushGeoJsonData(
 						{ url: $scope.inputs.url, 
-						  source_type: $scope.inputs.sourceType,
+						  source_type: $scope.inputs.sourceType.key,
 						  map_type: constructMapRules(),
 						  harvesting_freq: $scope.inputs.harvesting_freq.value,
 						  lifetime: lifetime,
@@ -153,12 +147,12 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 				} else {
 					PushService.pushCustomData(
 						{ url: $scope.inputs.url, 
-						  source_type: $scope.inputs.sourceType,
+						  source_type: $scope.inputs.sourceFormat,
 						  map_type: constructMapRules(),
 						  harvesting_freq: $scope.inputs.harvesting_freq.value,
 						  lifetime: lifetime,
 						  public: $scope.inputs.public
-						}, $scope.sourceTypeList[$scope.inputs.sourceType].endpoint).then(function(data) {
+						}, $scope.sourceTypeList[$scope.inputs.sourceFormat].endpoint).then(function(data) {
 						$scope.messages.error = '';
 						$scope.messages.success = data.known + ' source(s) known, ' + data['new'] + ' new source(s) added';
 					}, function(err, status) {
@@ -189,17 +183,17 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 					return;
 				}
 
-				if (!$scope.inputs.sourceType) {
+				if (!$scope.inputs.sourceFormat) {
 					$scope.messages.validateError = 'Please select a source type';
 				}
 				$scope.validateStatus = 'waiting';
-				PushService.validate($scope.inputs.url, $scope.inputs.sourceType).then(function(data) {
+				PushService.validate($scope.inputs.url, $scope.inputs.sourceFormat).then(function(data) {
 					if (data.status == 'offline') {
 						$scope.validateStatus = 'error';
 						$scope.messages.validateError = 'The provided url is unreachable.';
 					} else if (data.status == 'invalid') {
 						$scope.validateStatus = 'error';
-						$scope.messages.validateError = 'Data format is not valid for source type ' + $scope.sourceTypeList[$scope.inputs.sourceType].name;
+						$scope.messages.validateError = 'Data format is not valid for source type ' + $scope.sourceTypeList[$scope.inputs.sourceFormat].name;
 					} else if (data.status = 'unsupported') {
 						$scope.validateStatus = '';
 						$scope.messages.validateError = '';
@@ -230,6 +224,26 @@ directivesModule.directive("addConnectionModal", ['$http', '$timeout', '$statePa
 			$scope.accessDataField = function(row) {
 				if ($scope.inputs.mapRules[row][0] && $scope.currentData) return accessDataField($scope.currentData, $scope.inputs.mapRules[row][0]);
 			}
+
+			$scope.clearModalData = function() {
+				$scope.currentData = null;
+				$scope.showCurrentData = false;
+				$scope.inputs = { mapRules : {}};
+				$scope.validateStatus = '';
+				$scope.messages = {};
+				// Default values
+				$scope.inputs.harvesting_freq = {'value': 360, 'label':'6 hours'};
+				$scope.inputs.public = true;
+
+				for (var key in $scope.loklakFields) {
+					if (!$scope.inputs.mapRules[key]) {
+						$scope.inputs.mapRules[key] = [];
+					}
+					$scope.inputs.mapRules[key][1] = $scope.loklakFields[key].value; // fill second column with loklak fields
+				}
+			};
+
+			$scope.clearModalData();
 		}
 	};
 }]);
