dc361e5f4738519ae6b85e3bcc9e2028042e89dc
Prashant Kiran <prasht63@gmail.com>
2015-08-25 02:24:25 +0530

    Added photos on the map

--- app/js/controllers/insta.js
+++ app/js/controllers/insta.js
@@ -33,7 +33,7 @@ var plotted=0;
         var basemapObj = {
             "First Basemap": grayscale
         };
-      var url="https://api.instagram.com/v1/users/self/feed?callback=JSON_CALLBACK&access_token=";
+      var url="https://api.instagram.com/v1/users/self/feed?callback=JSON_CALLBACK&count=20&access_token=";
       var map2 =L.map('map2',{layers:[grayscale,photoslayer]}).setView([20,0], 2);
       var access_token;
       var query = window.location.href;
@@ -41,17 +41,108 @@ var plotted=0;
       var pairs=vars[1].split("=");
       access_token=pairs[1];
       url=url+access_token;
-    console.log(url);
-    $http.jsonp(url).then(function(response) {
-       console.log(response);  }, function(response) {
+      var i;
+      for(i=0;i<10;i++)
+      {
+        console.log("url being used"+url)
+      $http.jsonp(url).then(function(response)
+      {   
+         var picdata=response.data.data;
+         picdata.forEach(function(ele){
+          if(ele.location)
+          {
+            if(ele.location.latitude)
+            {
+              $scope.photos.push(ele);
+            }
+          }
+         })
+        // console.log(url)
+         url=response.data.pagination.next_url; 
+        // console.log(url);
+        // console.log(url);
+         //plotOnMap();
+      }, function(response)
+         {
+            console.log(response);
+         });
 
-      console.log(response);
-  });
+    }
+
+
+    
+
+
   $scope.curr_page=0;
   $scope.lastpage;
  
 
-   
+   function plotOnMap()
+   {
+   // console.log("called");
+    var photosMarker = {
+                "type": "FeatureCollection",
+                "features": []
+            };
+    $scope.photos.forEach(function(ele){
+
+
+      
+      var pointObject = {
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            ele.location.latitude,
+            ele.location.longitude
+                         ]
+          },
+       "type": "Feature",
+       "properties": {
+          "popupContent" : "<div class='foobar'><img src="+ele.images.standard_resolution.url+"></div>" ,
+          "propic" : ele.images.thumbnail.url
+        },
+                        "id": "123"
+      };
+      photosMarker.features.push(pointObject);
+
+                    });
+      add_marker(photosMarker);
+   }
+
+  function add_marker(result) {
+                    
+                   // console.log("adding markers");
+                    var i;
+                    for (i = 0; i < result.features.length; i++) {
+                      console.log(i);
+                        //console.log(result.features[i].propic-url);
+                        var tweetIcon = L.icon({
+                        iconUrl: result.features[i].properties.propic ,
+                        iconSize:     [30, 30]
+                    });
+                        var lat = result.features[i].geometry.coordinates[0];
+                        var lng = result.features[i].geometry.coordinates[1];
+                        console.log(lat+" , "+lng+" plotted");
+                        marker[i] = new L.Marker([lat, lng], {
+                            id: i,
+                            icon: tweetIcon
+                        });
+                        
+                        
+                            marker[i].addTo(photoslayer);
+                            
+                        
+                        
+                        var popup = L.popup({
+                            autoPan: true
+                        }).setContent(result.features[i].properties.popupContent);
+                        marker[i].bindPopup(popup);
+                        plotted++;
+                        
+                        
+                    }
+                    
+                }
       
 
 
